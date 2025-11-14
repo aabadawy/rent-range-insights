3cdc619485edcb12d5ff82f1a14b415e7c3072c4
ahmedbadawy <ahmedbadawy.fcai@gmail.com>
2025-11-14 19:11:46 +0200

    wip

--- tests/Unit/ValueObjects/MoneyTest.php
+++ tests/Unit/ValueObjects/MoneyTest.php
@@ -3,7 +3,7 @@
 use App\ValueObjects\Money;
 
 test('it saves float money should be converted to integer', function () {
-    expect(($money = Money::make(123.456789))->amount())
+    expect(($money = Money::make(123.456789,false))->amount())
         ->toBe(1234567)
         ->and($money->euro())->toBe(123.4567);
 });
